8f6f63f0d6120ea12bbcf9ced063c8300686f86e
nanosphere <nanosphere12@gmail.com>
2016-05-26 01:35:24 +0900

    update

---
+++ Assets/CubeSpin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSpin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate(1,0,0);
+	}
+}
